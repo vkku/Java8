6f674bdc37adcbdf2e7a38c3a108a43fd028fd03
vkku <kumar.vivekten@gmail.com>
2019-07-13 23:49:21 +0530

    RxJava - Observable.scan()

--- src/main/java/StreamLambda.java
+++ src/main/java/StreamLambda.java
@@ -1,27 +1,39 @@
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertTrue;
+import io.reactivex.Observable;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
-
-import static java.util.stream.Collectors.toList;
+import org.junit.Test;
 
 public class StreamLambda {
 
-    public List<String> zap(List<String> list, String omit) {
+  public List<String> zap(List<String> list, String omit) {
 
-        return list
-                .stream()
-                .filter(not(omit::equals))
-                .collect(toList());
-    }
+    return list.stream().filter(not(omit::equals)).collect(toList());
+  }
 
 
-    public static <T> Predicate<T> not(Predicate<T> p) {
-        return p.negate();
-    }
+  public static <T> Predicate<T> not(Predicate<T> p) {
+    return p.negate();
+  }
 
-    public static void main(String[] args) {
-        StreamLambda streamLambda = new StreamLambda();
-        System.out.println(streamLambda.zap(Arrays.asList("java", "pascal", "c", "lisp", "cpp"), "java"));
-    }
+  public static void main(String[] args) {
+    StreamLambda streamLambda = new StreamLambda();
+    System.out
+        .println(streamLambda.zap(Arrays.asList("java", "pascal", "c", "lisp", "cpp"), "java"));
+    StreamLambda lambda = new StreamLambda();
+    lambda.rxScan();
+  }
+
+  String result = "";
+
+  @Test
+  public void rxScan() {
+    String[] letters = {"a", "b", "c"};
+    Observable.fromArray(letters).scan(new StringBuilder(), StringBuilder::append)
+    .subscribe(total -> result += total.toString());
+    assertTrue(result.equals("aababc"));
+  }
 
 }
