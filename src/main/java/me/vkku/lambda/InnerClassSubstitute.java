b9a2ecdd4a047851c1f81e693b8bade402c55e33
vkku <kumar.vivekten@gmail.com>
2019-07-14 14:51:01 +0530

    Simple filter and collect

--- src/main/java/me/vkku/lambda/InnerClassSubstitute.java
+++ src/main/java/me/vkku/lambda/InnerClassSubstitute.java
@@ -1,12 +1,15 @@
 package me.vkku.lambda;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.Test;
 
 interface UseLambda {
   public abstract void printInner(String str);
 
   // TODO : Make it work
-  // public abstract void printInnerAgain(Integer val);
+  // public abstract void printInnerAgain(int val);
 }
 
 
@@ -22,6 +25,13 @@ public class InnerClassSubstitute {
     };
 
     lambdaa.printInner("Works !");
+  }
+
+  @Test
+  public void simpleLemon() {
+    List lemonList = Arrays.asList("easy", "peasy", "lemon", "squeezy");
+    System.out.println(lemonList.stream().filter(str -> str.equals("lemon"))
+        .collect(Collectors.toList()));
   }
 
 }
